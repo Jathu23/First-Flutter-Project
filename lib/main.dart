975f87d6c70dcd2e53ecc9e1534fc84da23f5577
Jathu 23 <jathushanj2003@gmail.com>
2025-02-01 21:56:06 +0530

    simple code

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: (Text("Hello flutter",
+      style: TextStyle(color: Colors.blue,
+      backgroundColor:Colors.limeAccent ),)),
+    )
+  );
+}
+
