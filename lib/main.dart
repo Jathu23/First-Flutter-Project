4318f0c17cbcb0f18d600ad115c7aaca5d9e0440
Jathu 23 <jathushanj2003@gmail.com>
2025-02-02 08:45:10 +0530

    simple appbar

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,13 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: (Text("Hello flutter",
-      style: TextStyle(
-      color: Colors.blue,
-      backgroundColor:Colors.limeAccent,
-      fontSize: 32.0,
-      fontWeight: FontWeight.bold, 
-      letterSpacing: 2.0, 
-      wordSpacing: 4.0, 
-      fontStyle: FontStyle.italic  ),
-      textAlign: TextAlign.center,)),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter"),
+          backgroundColor: Colors.blue,
+          centerTitle: false,
+        ),
+      )
       
     )
   );
