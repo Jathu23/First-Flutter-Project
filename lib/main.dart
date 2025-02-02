41ef8346db614f5fc69027d862b3a0b2e5243223
Jathu 23 <jathushanj2003@gmail.com>
2025-02-02 19:23:19 +0530

    StatefulWidget example

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,32 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MainApp()
-  );
+  runApp(MaterialApp(home: Counter()));
 }
 
-class MainApp extends StatelessWidget {
+class Counter extends StatefulWidget {
+  @override
+  _CounterState createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-    home: Container(
-      color: Colors.lightBlue,
-      child: Text("hello"),
-    )
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('$_counter', style: TextStyle(fontSize: 48)),
+            ElevatedButton(
+              onPressed: () => setState(() => _counter++),
+              child: Text('Increment'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
