057d3cb5644e88a18579b0b218ed66910d13276a
Jathu 23 <jathushanj2003@gmail.com>
2025-02-02 08:36:04 +0530

    more text styling

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,16 @@ void main() {
   runApp(
     MaterialApp(
       home: (Text("Hello flutter",
-      style: TextStyle(color: Colors.blue,
-      backgroundColor:Colors.limeAccent ),)),
+      style: TextStyle(
+      color: Colors.blue,
+      backgroundColor:Colors.limeAccent,
+      fontSize: 32.0,
+      fontWeight: FontWeight.bold, 
+      letterSpacing: 2.0, 
+      wordSpacing: 4.0, 
+      fontStyle: FontStyle.italic  ),
+      textAlign: TextAlign.center,)),
+      
     )
   );
 }
