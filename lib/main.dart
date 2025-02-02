44c6bc6aaac69fc1534d161ca039f62e8b4ea97d
Jathu 23 <jathushanj2003@gmail.com>
2025-02-02 17:28:42 +0530

    create StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -3,85 +3,14 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-       appBar: AppBar(
-        title: Text("App Bar"),
-        centerTitle: false,
-        backgroundColor: Colors.blue,
-       ),
-       body:Column(
-        children: [
-           Container(
-        width: double.infinity,
-        height: 150,
-        decoration:BoxDecoration(
-  color: Colors.white,
-  borderRadius: BorderRadius.circular(20),
-  boxShadow: [
-    BoxShadow(
-      color: Colors.black26, 
-      spreadRadius: 3,
-      blurRadius: 10,
-      offset: Offset(2, 2), 
-    ),
-  ],
-),
-margin: EdgeInsets.all(21),
-padding: EdgeInsets.all(21),
-        
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Item 1", style: TextStyle(
-              fontSize: 21,
-              fontWeight: FontWeight.bold
-            ),),
-            Text("Deceription"),
-            Text("Price  28.89",style: TextStyle(
-              fontSize: 21,
-              fontWeight: FontWeight.bold
-            ),),
-          ],
-        ),
-       ),
-        Container(
-        width: double.infinity,
-        height: 150,
-        decoration:BoxDecoration(
-  color: Colors.white,
-  borderRadius: BorderRadius.circular(20),
-  boxShadow: [
-    BoxShadow(
-      color: Colors.black26, 
-      spreadRadius: 3,
-      blurRadius: 10,
-      offset: Offset(2, 2), 
-    ),
-  ],
-),
-margin: EdgeInsets.all(21),
-padding: EdgeInsets.all(21),
-        
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Item 2", style: TextStyle(
-              fontSize: 21,
-              fontWeight: FontWeight.bold
-            ),),
-            Text("Deceription"),
-            Text("Price  59.54",style: TextStyle(
-              fontSize: 21,
-              fontWeight: FontWeight.bold
-            ),),
-          ],
-        ),
-       )
-        ],
-       )
-      )
-      
+    home: MainApp(), 
     )
   );
 }
 
+class MainApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text("hello");
+  }
+}
