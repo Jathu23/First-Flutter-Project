b509feb10f595b8540eb4c8e2af731c924335827
Jathu 23 <jathushanj2003@gmail.com>
2025-02-02 16:56:16 +0530

    simple stack using column

--- lib/main.dart
+++ lib/main.dart
@@ -4,45 +4,81 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Center(
-    child: Container(
-      width:300,
-      height: 500,
-      decoration: BoxDecoration(
-        color: Colors.blue,
-        borderRadius:BorderRadius.circular(20),
-        
-      ),
-      
-      
-     child: Row(
-      mainAxisAlignment: MainAxisAlignment.end,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-       Container(
-        width: 100,
+       appBar: AppBar(
+        title: Text("App Bar"),
+        centerTitle: false,
+        backgroundColor: Colors.blue,
+       ),
+       body:Column(
+        children: [
+           Container(
+        width: double.infinity,
         height: 150,
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-            color: Colors.amber,
-            borderRadius: BorderRadius.circular(30)
-        ),
-        child: Text("Hello Flutter"),
-      ),
-        Container(
-        width: 100,
-        height: 150,
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-            color: Colors.amber,
-            borderRadius: BorderRadius.circular(30)
-        ),
-        child: Text("Hello Flutter"),
-      )
-      ],
-     ),
+        decoration:BoxDecoration(
+  color: Colors.white,
+  borderRadius: BorderRadius.circular(20),
+  boxShadow: [
+    BoxShadow(
+      color: Colors.black26, 
+      spreadRadius: 3,
+      blurRadius: 10,
+      offset: Offset(2, 2), 
     ),
+  ],
+),
+margin: EdgeInsets.all(21),
+padding: EdgeInsets.all(21),
+        
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Item 1", style: TextStyle(
+              fontSize: 21,
+              fontWeight: FontWeight.bold
+            ),),
+            Text("Deceription"),
+            Text("Price  28.89",style: TextStyle(
+              fontSize: 21,
+              fontWeight: FontWeight.bold
+            ),),
+          ],
         ),
+       ),
+        Container(
+        width: double.infinity,
+        height: 150,
+        decoration:BoxDecoration(
+  color: Colors.white,
+  borderRadius: BorderRadius.circular(20),
+  boxShadow: [
+    BoxShadow(
+      color: Colors.black26, 
+      spreadRadius: 3,
+      blurRadius: 10,
+      offset: Offset(2, 2), 
+    ),
+  ],
+),
+margin: EdgeInsets.all(21),
+padding: EdgeInsets.all(21),
+        
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Item 2", style: TextStyle(
+              fontSize: 21,
+              fontWeight: FontWeight.bold
+            ),),
+            Text("Deceription"),
+            Text("Price  59.54",style: TextStyle(
+              fontSize: 21,
+              fontWeight: FontWeight.bold
+            ),),
+          ],
+        ),
+       )
+        ],
+       )
       )
       
     )
