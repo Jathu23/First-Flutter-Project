1a0c1beea2d14f58f489545beafbb889bb7bab18
Jathu 23 <jathushanj2003@gmail.com>
2025-02-04 21:03:06 +0530

    simple list adding

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,71 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(todo());
+  runApp(TodoApp());
 }
 
-class todo extends StatefulWidget{
+class TodoApp extends StatefulWidget {
   @override
-  State<todo> createState() => _todoState();
+  State<TodoApp> createState() => _TodoAppState();
 }
 
-class _todoState extends State<todo> {
-  String listdata = "No List Items";
+class _TodoAppState extends State<TodoApp> {
+  List<String> itemList = ["first"];
   TextEditingController listdatacontroller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-   
-    return MaterialApp(home: (
-      Scaffold(
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Todo List'),
+        ),
         body: Column(
           children: [
-      Container(
-        padding: EdgeInsets.all(20),
-        child:       TextField(
-          controller: listdatacontroller,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                label: Text("Enter List")),
+            Container(
+              padding: EdgeInsets.all(20),
+              child: TextField(
+                controller: listdatacontroller,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Enter List Item",
+                ),
+              ),
             ),
-      ),
-      MaterialButton(onPressed:  () {
-        setState(() {
-          listdata = listdatacontroller.text;
-        });
-      } ,
-      onLongPress: () {
-        setState(() {
-           listdata = "long presed";
-        });
-      },
-      padding: EdgeInsets.all(30),
-      color: Colors.lightBlueAccent,
-      shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(30) ),
-      
-      child: Text("Click"),
-      ),
-      Text(listdata)
+            MaterialButton(
+              onPressed: () {
+                setState(() {
+                  if (listdatacontroller.text.isNotEmpty) {
+                    itemList.add(listdatacontroller.text);
+                    listdatacontroller.clear();
+                  }
+                });
+              },
+              onLongPress: () {
+                setState(() {
+                  itemList = [];
+                });
+              },
+              padding: EdgeInsets.all(30),
+              color: Colors.lightBlueAccent,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Text("Add Item"),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: itemList.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(itemList[index]),
+                  );
+                },
+              ),
+            ),
           ],
-
-        ) ,)
-    ));
+        ),
+      ),
+    );
   }
 }
