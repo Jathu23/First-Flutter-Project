8884d6d1775aa49704a14d60c95040ec26ec202e
Jathu 23 <jathushanj2003@gmail.com>
2025-02-04 21:40:42 +0530

    add delete function

--- lib/main.dart
+++ lib/main.dart
@@ -22,49 +22,64 @@ class _TodoAppState extends State<TodoApp> {
         ),
         body: Column(
           children: [
-     Row(
-    children: [
-      Expanded(child: 
-             Container(
-              padding: EdgeInsets.all(20),
-              child: TextField(
-                controller: listdatacontroller,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Enter List Item",
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    child: TextField(
+                      controller: listdatacontroller,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Enter List Item",
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-            ),),
-            
-            MaterialButton(
-              onPressed: () {
-                setState(() {
-                  if (listdatacontroller.text.isNotEmpty) {
-                    itemList.add(listdatacontroller.text);
-                    listdatacontroller.clear();
-                  }
-                });
-              },
-              onLongPress: () {
-                setState(() {
-                  itemList = [];
-                });
-              },
-              padding: EdgeInsets.all(30),
-              color: Colors.lightBlueAccent,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-              child: Text("Add Item"),
+                MaterialButton(
+                  onPressed: () {
+                    setState(() {
+                      if (listdatacontroller.text.isNotEmpty) {
+                        itemList.add(listdatacontroller.text);
+                        listdatacontroller.clear();
+                      }
+                    });
+                  },
+                  onLongPress: () {
+                    setState(() {
+                      itemList = [];
+                    });
+                  },
+                  padding: EdgeInsets.all(30),
+                  color: Colors.lightBlueAccent,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Text("Add Item"),
+                ),
+              ],
             ),
-    ],
-     ),
             Expanded(
               child: ListView.builder(
                 itemCount: itemList.length,
                 itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(itemList[index]),
+                  return Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          padding: EdgeInsets.all(20),
+                          child: Text(itemList[index]),
+                        ),
+                      ),
+                      MaterialButton(
+                        onPressed: () {
+                          setState(() {
+                            itemList.removeAt(index);
+                          });
+                        },
+                        child: Icon(Icons.delete, color: Colors.red),
+                      )
+                    ],
                   );
                 },
               ),
